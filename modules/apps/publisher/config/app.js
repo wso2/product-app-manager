5b3f8b7f652e8a5ff787840435c922a84410e3b4
nuwanbando <nuwan@bandara.co>
2013-08-08 15:36:09 +0530

    Publisher login code

--- modules/apps/publisher/config/app.js
+++ modules/apps/publisher/config/app.js
@@ -1,27 +1,43 @@
 /*
-	Description: Initialization script
-	Filename:app.js
-	Created Date: 29/7/2013
-*/
+ Description: Initialization script
+ Filename:app.js
+ Created Date: 29/7/2013
+ */
 
-var caramel=require('caramel'),
-	rxt_management=require('/modules/rxt.manager.js').rxt_management(),
-	route_management=require('/modules/router-g.js').router(),
-	config=require('/config/publisher.json'),	
-	carbon=require('carbon'),
-	url='https://localhost:9443/admin',
-	username='admin',
-	password='admin',
-	mediaType='application/vnd.wso2.registry-ext-type+xml'; //TODO: Change the url
+var caramel = require('caramel'),
+    rxt_management = require('/modules/rxt.manager.js').rxt_management(),
+    route_management = require('/modules/router-g.js').router(),
+    carbon = require('carbon'),
+    mediaType = 'application/vnd.wso2.registry-ext-type+xml', //TODO: Change the url
+    conf = carbon.server.loadConfig('carbon.xml'),
+    offset = conf.*::['Ports'].*::['Offset'].text(),
+    hostName = conf.*::['HostName'].text().toString();
 
-var server=new carbon.server.Server(url);
-var registry=new carbon.registry.Registry(server,{
-		systen:true,
-		username:username,
-		tenantId:carbon.server.superTenant.tenantId
-});
-var rxtManager=new rxt_management.RxtManager(registry);
-var routeManager=new route_management.Router();
+if (hostName === null || hostName === '') {
+    hostName = 'localhost';
+}
+
+var httpPort = 9763 + parseInt(offset, 10);
+var httpsPort = 9443 + parseInt(offset, 10);
+
+var process = require('process');
+process.setProperty('server.host', hostName);
+process.setProperty('http.port', httpPort.toString());
+process.setProperty('https.port', httpsPort.toString());
+
+var config = require('/config/publisher.js').config();
+
+var server = require('/modules/server.js');
+
+server.init(config);
+
+var user = require('/modules/user.js');
+user.init(config);
+
+//var server = new carbon.server.Server(url);
+var registry = server.systemRegistry();
+var rxtManager = new rxt_management.RxtManager(registry);
+var routeManager = new route_management.Router();
 
 
 routeManager.setRenderer(config.router.RENDERER);
@@ -30,18 +46,18 @@ routeManager.setRenderer(config.router.RENDERER);
 //a RxtTemplate(Refer rxt.domain.js)
 rxtManager.loadAssets();
 
-application.put(config.app.RXT_MANAGER,rxtManager);
-application.put(config.app.ROUTE_MANAGER,routeManager);
+application.put(config.app.RXT_MANAGER, rxtManager);
+application.put(config.app.ROUTE_MANAGER, routeManager);
 
 //Configure Caramel
 caramel.configs({
-	context:'/publisher',
-	cache:true,
-	negotiation:true,
-	themer:function(){
-		//TODO: Hardcoded theme
-		return 'publisher';
-	}
+    context: '/publisher',
+    cache: true,
+    negotiation: true,
+    themer: function () {
+        //TODO: Hardcoded theme
+        return 'publisher';
+    }
 
 });
 
--- modules/apps/publisher/themes/publisher/renderers/controllers/assets_router.js
+++ modules/apps/publisher/themes/publisher/renderers/controllers/assets_router.js
@@ -1,15 +1,21 @@
 /*
-	Description: Renders the assets.jag view
-	Filename:assets.js
-	Created Date: 29/7/2013
-*/
-var render=function(theme,data,meta,require){
-	theme('single-col-fluid', {
+ Description: Renders the assets.jag view
+ Filename:assets.js
+ Created Date: 29/7/2013
+ */
+var render = function (theme, data, meta, require) {
+    theme('single-col-fluid', {
         title: data.title,
+        header: [
+            {
+                partial: 'header',
+                context: data
+            }
+        ],
         ribbon: [
             {
                 partial: 'ribbon',
-		context:data
+                context: data
             }
         ],
         leftnav: [
@@ -20,7 +26,7 @@ var render=function(theme,data,meta,require){
         listassets: [
             {
                 partial: 'list-assets',
-		context: data
+                context: data
             }
         ]
     });
