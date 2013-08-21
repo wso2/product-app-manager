d79fd20470bf5f2f26622cba46d98fd2f8c11eb0
ruchiraw <ruchira.wageesha@gmail.com>
2013-08-21 08:23:21 +0530

    fixing rxt initialization issue

--- modules/apps/publisher/modules/publisher.js
+++ modules/apps/publisher/modules/publisher.js
@@ -4,7 +4,7 @@
             GovernanceConstants = org.wso2.carbon.governance.api.util.GovernanceConstants,
             carbon = require('carbon'), server = require('/modules/server.js'),
             reg = server.systemRegistry(), um = server.userManager();
-        CommonUtil.addRxtConfigs(reg.registry, reg.tenantId);
+        CommonUtil.addRxtConfigs(reg.registry.getChrootedRegistry("/_system/governance"), reg.tenantId);
         um.authorizeRole(carbon.user.anonRole, GovernanceConstants.RXT_CONFIGS_PATH, carbon.registry.actions.GET);
     };
 }());
