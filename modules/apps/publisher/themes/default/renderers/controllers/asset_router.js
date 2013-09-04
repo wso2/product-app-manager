c90e4c46fec619c27802e3047182f435c310ad9f
chamathb <chamathb@live.com>
2013-09-04 23:05:28 +0530

    adding responsive left menu

--- modules/apps/publisher/themes/default/renderers/controllers/asset_router.js
+++ modules/apps/publisher/themes/default/renderers/controllers/asset_router.js
@@ -3,6 +3,57 @@
 	Filename:asset.js
 	Created Date: 29/7/2013
 */
+var breadcrumbData = { breadcrumb :
+    [
+        {
+            assetType : "Gadget",
+            url : "/publisher/assets/gadget/",
+            assetIcon : "icon-dashboard", //font-awesome icon class
+            assets : [
+                {assetName : "Bar Chart", "versions" : ["1.0.0", "2.0.0", "2.0.1"]},
+                {assetName : "Line Chart", "versions" : ["1.0.2", "1.0.4", "3.1.1"]},
+                {assetName : "Horizontal Bar Chart", "versions" : ["1.1.2", "1.4.1", "2.1.1"]}
+            ]
+        },
+        {
+            assetType : "EBook",
+            url : "/publisher/assets/ebook/",
+            assets : [
+                {assetName : "The art of Unix Programming", "versions" : ["Edition-1", "Edition-2", "Edition-3"]},
+                {assetName : "The Complete Works of H.P.Lovecraft", "versions" : ["Edition-1", "Edition-2", "Edition-3"]}
+            ]
+        }
+    ]
+};
+var leftNavItems = { leftNavLinks :
+    [
+        {
+            name : "Browse All",
+            additionalClasses : "prominent-link",
+            url : "/publisher/assets/gadget/"
+        },
+        {
+            name : "Overview",
+            iconClass : "icon-list-alt",
+            url : "#"
+        },
+        {
+            name : "Edit",
+            iconClass : "icon-edit",
+            url : "#"
+        },
+        {
+            name : "Life Cycle",
+            iconClass : "icon-retweet",
+            url : "#"
+        },
+        {
+            name : "Versions",
+            iconClass : "icon-qrcode",
+            url : "#"
+        }
+    ]
+};
 var render=function(theme,data,meta,require){
 
 	var listPartial='view-asset';
@@ -29,13 +80,13 @@ var render=function(theme,data,meta,require){
         ribbon: [
             {
                 partial: 'ribbon',
-		        context:data
+		        context:breadcrumbData
             }
         ],
         leftnav: [
             {
                 partial: 'left-nav',
-                context: data
+                context: leftNavItems
             }
         ],
         listassets: [
--- modules/apps/publisher/themes/default/renderers/controllers/assets_router.js
+++ modules/apps/publisher/themes/default/renderers/controllers/assets_router.js
@@ -3,6 +3,44 @@
  Filename:assets.js
  Created Date: 29/7/2013
  */
+var breadcrumbData = { breadcrumb :
+    [
+        {
+            assetType : "Gadget",
+            url : "/publisher/assets/gadget/",
+            assetIcon : "icon-dashboard" //font-awesome icon class
+        },
+        {
+            assetType : "EBook",
+            url : "/publisher/assets/ebook/",
+            assetIcon : "icon-book" //font-awesome icon class
+        },
+        {
+            assetType : "Site",
+            url : "/publisher/assets/site/",
+            assetIcon : "icon-compass" //font-awesome icon class
+        }
+    ]
+};
+var leftNavItems = { leftNavLinks :
+    [
+        {
+            name : "Browse All",
+            additionalClasses : "prominent-link",
+            url : "/publisher/assets/gadget/"
+        },
+        {
+            name : "Add ebook",
+            iconClass : "icon-plus-sign-alt",
+            url : "/publisher/asset/ebook"
+        },
+        {
+            name : "Statistics",
+            iconClass : "icon-dashboard",
+            url : "#"
+        }
+    ]
+};
 var render = function (theme, data, meta, require) {
     theme('single-col-fluid', {
         title: data.title,
@@ -15,13 +53,13 @@ var render = function (theme, data, meta, require) {
         ribbon: [
             {
                 partial: 'ribbon',
-                context: data
+                context: breadcrumbData
             }
         ],
         leftnav: [
             {
                 partial: 'left-nav',
-                context: data
+                context: leftNavItems
             }
         ],
         listassets: [
