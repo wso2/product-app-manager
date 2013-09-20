c44fbd31f184342eca11eb56b2ebee2b990d15a9
manu <manu@wso2.com>
2013-09-21 00:30:08 +0530

    init by putting a dummy activity. for creating the event space.

--- modules/apps/social/app.js
+++ modules/apps/social/app.js
@@ -8,3 +8,12 @@ caramel.configs({
         return 'default';
     }
 });
+
+var carbon = require('carbon');
+var social = carbon.server.osgiService('org.wso2.carbon.social.service.SocialActivityService');
+setTimeout(function () {
+    social.publish({"verb": "post",
+        "object": {"type": "comment", "content": "test"},
+        "target": {"id": "test_"}
+    });
+}, 10000)
