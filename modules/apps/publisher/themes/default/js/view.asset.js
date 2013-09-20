2e698c18ccef42cb88b24e66fa7844332585b544
sameeram <mgjaye@gmail.com>
2013-09-20 11:10:40 +0530

    provided version section for the view asset

---
+++ modules/apps/publisher/themes/default/js/view.asset.js
@@ -0,0 +1,18 @@
+/*
+Description: The script is used to provide version support.It allows a user to specify a new version of the
+             currently viewing asset.The script also checks if the version entered by the user does not exist already.
+             If it exists the user is informed that the version already exists.
+Filename:view.asset.js
+Created Date:  20/8/2013
+ */
+
+$(function(){
+
+
+    $('btn-increase-version-asset').on('click',function(){
+
+           alert('created a new version');
+
+
+    });
+});
