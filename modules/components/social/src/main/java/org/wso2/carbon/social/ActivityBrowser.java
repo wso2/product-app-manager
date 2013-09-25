61720b1a86d0817f5a040c9a22cc66a3934a1a4f
manu <manu@wso2.com>
2013-09-25 14:30:00 +0530

    chronological sorting of activities.

--- modules/components/social/src/main/java/org/wso2/carbon/social/ActivityBrowser.java
+++ modules/components/social/src/main/java/org/wso2/carbon/social/ActivityBrowser.java
@@ -1,20 +1,22 @@
 package org.wso2.carbon.social;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class ActivityBrowser {
     private static final Log LOG = LogFactory.getLog(ActivityBrowser.class);
     private static final String STREAM_NAME = ActivityPublisher.STREAM_NAME.replaceAll("\\.", "_");
+    private JsonParser parser = new JsonParser();
+
     private Connection conn;
 
     public List<String> listActivities(String targetId) {
-        List<String> result = null;
+        List<Activity> activities = null;
         Connection connection = getConnection();
         if (connection != null) {
             PreparedStatement statement = null;
@@ -23,9 +25,11 @@ public class ActivityBrowser {
                 statement = connection.prepareStatement("SELECT * FROM " + STREAM_NAME + " WHERE 'payload_target.id'=?");// WHERE target.id=?
                 statement.setString(1, targetId);
                 resultSet = statement.executeQuery();
-                result = new ArrayList<String>();
+                activities = new ArrayList<Activity>();
                 while (resultSet.next()) {
-                    result.add(resultSet.getString("payload_body"));
+                    JsonElement body = parser.parse(resultSet.getString("payload_body"));
+                    Activity activity = new Activity(body.getAsJsonObject(), resultSet.getInt("Timestamp"));
+                    activities.add(activity);
                 }
             } catch (SQLException e) {
                 LOG.error("Can't retrieve activities form cassandra.", e);
@@ -42,8 +46,28 @@ public class ActivityBrowser {
                 }
             }
         }
+        sortChronologically(activities);
 
-        return result;
+        return serializeEach(activities);
+    }
+
+    private List<String> serializeEach(List<Activity> activities) {
+        List<String> stringList = new ArrayList<String>(activities.size());
+        for (Activity activity : activities) {
+            stringList.add(activity.getBody().toString());
+        }
+        return stringList;
+    }
+
+    public void sortChronologically(List<Activity> activities) {
+        if (activities != null) {
+            Collections.sort(activities, new Comparator<Activity>() {
+                @Override
+                public int compare(Activity o1, Activity o2) {
+                    return o1.getTimestamp() - o2.getTimestamp();
+                }
+            });
+        }
     }
 
     public void makeIndexes(String column) {
