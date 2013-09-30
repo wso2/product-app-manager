852c019d659efba115e47f15cffc21fa83f06838
sameeram <mgjaye@gmail.com>
2013-09-30 11:49:57 +0530

    changed the add asset form to transfer files

--- modules/apps/publisher/themes/default/js/logic/create.asset.js
+++ modules/apps/publisher/themes/default/js/logic/create.asset.js
@@ -12,18 +12,23 @@ $(function(){
 		$('#btn-create-asset').on('click',function(){
 			var fields=$('#form-asset-create :input');
 			var data={};
+            var formData=new FormData();
 			fields.each(function(){
 				if(this.type!='button')
 				{
 					//console.log(this.value);
 					data[this.id]=this.value;
+                    formData=fillForm(this,formData);
 				}
 			});
 
 			$.ajax({
 				url:'/publisher/asset/'+type,
 				type:'POST',
-				data: data,
+				data: formData,
+                cache:false,
+                contentType:false,
+                processData:false,
 				success:function(response){
 					alert('asset added.');
 					window.location='/publisher/assets/'+type+'/';
@@ -39,4 +44,26 @@ $(function(){
 
 		});
 	//}
+
+
+    /*
+    The function is used to add a given field to a FormData element
+    @field: The field to be added to the formData
+    @formData: The FormDara object used to store the field
+    @return: A FormData object with the added field
+     */
+    function fillForm(field,formData){
+
+        var fieldType=field.type;
+
+        if(fieldType=='file'){
+           console.log('added '+field.id+' file.');
+           formData.append(field.id,field.files[0]);
+        }
+        else{
+           formData.append(field.id,field.value);
+        }
+
+        return formData;
+    }
 });
