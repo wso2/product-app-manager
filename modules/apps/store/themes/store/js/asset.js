4c09640a8d3606e03bb7e9775b69987bb5995696
Praveena <praveenaj@gmail.com>
2013-09-30 17:50:36 +0530

    automatically adjust height of user review box according to content

--- modules/apps/store/themes/store/js/asset.js
+++ modules/apps/store/themes/store/js/asset.js
@@ -49,7 +49,8 @@ $(function () {
     });
 
     var loadReviews = function (url, page) {
-        var el = $('#tab-reviews').find('.content');
+        var iframe, iframeHeight, 
+        	el = $('#tab-reviews').find('.content');
         comments.updated = new Date().getTime();
         async.parallel({
             comments: function (callback) {
@@ -89,6 +90,10 @@ $(function () {
                 }, function (err, result) {
                     theme.loaded(el, result.comments);
                     el.append(result.paging);
+                    
+                    iframe = $('#tab-reviews').find('iframe');
+        			iframeHeight =  iframe.contents().height();
+        			iframe.height(iframeHeight); 
                 });
             }
             $('#assetp-tabs').tab();
