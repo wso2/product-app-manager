48dbc7daeb08be4f2b6dfb3ded1b8c45624c8359
nuwanbando <nuwan@bandara.co>
2013-09-30 19:53:47 +0530

    appjs fixes

--- modules/apps/sso/app.js
+++ modules/apps/sso/app.js
@@ -5,3 +5,22 @@ server.init(configs);
 
 var user = require('/modules/user.js');
 user.init(configs);
+
+var event = require('/modules/event.js');
+
+event.on('tenantLoad', function (tenantId) {
+
+    var log = new Log();
+    log.info('======================================================================================');
+    event.emit('tenantCreate', tenantId)
+    var server = require('/modules/server.js');
+    var user = require('/modules/user.js');
+    var config = server.configs(tenantId);
+    config[user.USER_OPTIONS] = {
+        "permissions": {
+            "login": {
+                "/permission/admin/login": ["ui.execute"]
+            }
+        }
+    };
+});
