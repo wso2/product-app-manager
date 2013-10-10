468e86e7f501b2eae111ca0b98c61b51880ef378
sameeram <mgjaye@gmail.com>
2013-10-10 18:17:06 +0530

    added a mandatory field check

--- modules/apps/publisher/modules/validations/validation.manager.js
+++ modules/apps/publisher/modules/validations/validation.manager.js
@@ -18,6 +18,7 @@ var validationManagement=function(){
 
     function ValidationManager(){
        this.validatorScripts=[];
+       this.init();
     }
 
     ValidationManager.prototype.init=function(){
@@ -25,47 +26,78 @@ var validationManagement=function(){
         var bundleManager=new bundler.BundleManager({path:VALIDATOR_PATH});
 
         var root=bundleManager.getRoot();
-
+        var that=this;
         root.each(function(bundle){
-
+           //log.info('looking at: '+bundle.getName());
             //Load the script
-            var file=require(VALIDATOR_PATH+bundle.getName()).validatorModule();
+            var file=require(VALIDATOR_PATH+'/'+bundle.getName()).validatorModule();
+            var validatorName=bundle.getName().replace(bundle.getExtension(),'');
 
-            this.validatorScripts.push(file);
+            that.validatorScripts.push({name:validatorName,script:file});
 
         });
     };
 
     /*
     The function runs all loaded validators
+    @context: The context of the validation request
+    @return: A report of the validator actions
      */
     ValidationManager.prototype.validate=function(context){
-        var validatorScript;
-        var kill=false;
+        var validator;
+        var noKill=false;
 
-        context['report']={};
-        context['report']['failedFields']=[];
+        //Create a report object to assist validators to report issues
+        context['report']=new Report();
+
+        log.debug('about to execute validators');
 
         for(var index in this.validatorScripts){
 
-            validatorScript=this.validatorScripts[index];
+            validator=this.validatorScripts[index];
 
             //Check if the current validator script can be run on the current context
-            if(validatorScript.isApplicable(context)){
+            if(validator.script.isApplicable(context)){
 
-                kill=validatorScript.execute(context);
+                noKill=validator.script.execute(context);
 
                 //Checks if the other validations should be skipped
-                if(kill){
-                    return context;
+                if(!noKill){
+                    log.debug('validation execution stopped by '+validator.name);
+                    return context.report.map;
                 }
 
             }
 
         }
 
-        return context;
+        return context.report.map;
     };
 
+    /*
+    The class is used to encapsulate the validation error reporting
+     */
+    function Report(){
+       this.map={};
+       this.map['failed']=false;
+    }
+
+    /*
+    Records an infraction for a given field.All infractions are grouped on a per field basis
+    @field: The field for which the infraction has been committed
+    @infraction: The form of validation infraction which has taken place
+     */
+    Report.prototype.record=function(field,infraction){
+
+        if(!this.map.hasOwnProperty(field)){
+            this.map[field]=[];
+        }
+        this.map.failed=true;
+        this.map[field].push(infraction);
+    }
+
+    return{
+        ValidationManager:ValidationManager
+    }
 
 }
