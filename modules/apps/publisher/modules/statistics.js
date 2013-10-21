3f3d28c71e186f2adbf7f981db43b1f61ea33adc
udarakr <udarakr@gmail.com>
2013-10-21 09:33:34 +0530

    resolving STORE-316

--- modules/apps/publisher/modules/statistics.js
+++ modules/apps/publisher/modules/statistics.js
@@ -1,6 +1,11 @@
 var db = new Database('WSO2_CARBON_DB');
+var currentTenant,o,
+    store = require('store'),
+    server = store.server;
 
-var getBookmarkStats = function (loggedInUser,domainName) {
+    currentTenant = server.tenant(o, session).tenantId;
+
+var getBookmarkStats = function (loggedInUser) {
 
     //Temporary fix as defautls assets are deployed as wso2.system.user
     /*if (loggedInUser == 'admin') {
@@ -10,7 +15,8 @@ var getBookmarkStats = function (loggedInUser,domainName) {
         "FROM REG_RESOURCE RS " +
         "JOIN REG_RESOURCE RR ON RS.REG_UUID=RR.REG_NAME " +
         "JOIN REG_PATH RP ON  RR.REG_PATH_ID = RP.REG_PATH_ID " +
-          "WHERE RS.REG_CREATOR = '" + loggedInUser + "@"+domainName+"' AND " +
+        "WHERE RS.REG_CREATOR = '" + loggedInUser + "' AND " +
+        "RR.REG_TENANT_ID = '" + currentTenant + "' AND " +
         "RP.REG_PATH_VALUE like '/_system/governance/users/%' AND " +
         "RR.REG_NAME IS NOT NULL GROUP BY RR.REG_NAME" ;
     return db.query(query);
@@ -18,11 +24,7 @@ var getBookmarkStats = function (loggedInUser,domainName) {
 };
 
 var getHotAssetStats = function (startDate, endDate) {
-    var currentTenant,o,
-        store = require('store'),
-        server = store.server;
 
-    currentTenant = server.tenant(o, session).tenantId;
     var query = "SELECT RR.REG_NAME AS asset_id, RS.REG_MEDIA_TYPE AS asset_type,COUNT(RR.REG_NAME) AS no_of_bookmarks " +
         "FROM REG_RESOURCE RS " +
         "JOIN REG_RESOURCE RR ON RS.REG_UUID=RR.REG_NAME " +
