1a158a4a754ebfd2bdbe1d8cd7db2d09edea0bf1
RaivU <ravi.undupitiya@gmail.com>
2014-06-09 16:18:28 +0530

    Updating UI so it does not show auto configurtion when it is not avialable

--- modules/apps/publisher/themes/appm/js/logic/create.asset.js
+++ modules/apps/publisher/themes/appm/js/logic/create.asset.js
@@ -294,8 +294,12 @@ $('#form-asset-create').ajaxSubmit(options);
           contentType: 'application/json',
           success: function(response) {
         	 
-        	  var providers_data = JSON.parse(response).response;
-        	  loadProviders(providers_data);
+        	  var providers_data = JSON.parse(response);
+              if((providers_data.success === true) && (!$.isEmptyObject(providers_data.response))) {
+            	  loadProviders(providers_data.response);
+              } else {
+                $("#ssoTable").remove();
+              }
   			
           },
           error: function(response) {
