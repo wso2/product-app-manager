aa0c91edcb647cae0569b7961ecfa75603a737ec
thilinis <thilinis@wso2.com>
2014-08-13 11:02:29 +0530

    password confirmation issue fix

--- modules/apps/store/themes/store/js/navigation.js
+++ modules/apps/store/themes/store/js/navigation.js
@@ -41,33 +41,42 @@ $(function() {
 	var register = function() {
 		if (!$("#form-register").valid())
 			return;
+
+		var username = $('#inp-username-register').val();
+		var password = $('#inp-password-register').val();
+		var confirmPassword = $('#inp-password-confirm').val();
 		
-		caramel.ajax({
-            		type: 'POST',
-            		url: '/apis/user/register',
-            		data: JSON.stringify({
-            			username : $('#inp-username-register').val(),
-   				password : $('#inp-password-register').val()
-            		}),
-            		success: function (data) {
-		        	if (!data.error) {
-				  	$('#messageModal').html($('#confirmation-data').html());
-				      	$('#messageModal h3.modal-title').html(('APP Store - Notification'));
-					$('#messageModal a.btn-primary').html('OK');
-					$('#messageModal div.modal-body').html();
-					$('#messageModal').modal();
-					$('#modal-register').modal('hide');
-					$('#messageModal a.btn-primary').click(function() {
-					$('#messageModal').modal('hide');
-					$('#modal-login').modal('show'); 
-				  	});
-		                } else {
-		        		showError(data.message);
-		        	}
-            		},
-            		contentType: 'application/json',
-            		dataType: 'json'
-        	});
+		if(password == confirmPassword){
+			
+			caramel.ajax({
+				type: 'POST',
+				url: '/apis/user/register',
+				data: JSON.stringify({
+					username : $('#inp-username-register').val(),
+					password : $('#inp-password-register').val()
+				}),
+				success: function (data) {
+					if (!data.error) {
+						$('#messageModal').html($('#confirmation-data').html());
+						$('#messageModal h3.modal-title').html(('APP Store - Notification'));
+						$('#messageModal a.btn-primary').html('OK');
+						$('#messageModal div.modal-body').html();
+						$('#messageModal').modal();
+						$('#modal-register').modal('hide');
+						$('#messageModal a.btn-primary').click(function() {
+							$('#messageModal').modal('hide');
+							$('#modal-login').modal('show'); 
+						});
+					} else {
+						showError(data.message);
+					}
+				},
+				contentType: 'application/json',
+				dataType: 'json'
+			});
+		}else{
+			showError("Password and Password Repeat do not match. Please re-enter.");
+		}
 	};
 
 	$('#btn-signout').live('click', function() {
