75f2832aad86fe0f063f8b22bb4d733af8f856de
Chatura Dilan <dilan@dilan.me>
2014-09-12 15:25:49 +0530

    Added Copy Asset UI

--- modules/apps/publisher/themes/appm/js/copy-app.js
+++ modules/apps/publisher/themes/appm/js/copy-app.js
@@ -1,3 +1,22 @@
 $( "#btnCopyApp" ).click(function() {
    var newVersion = $('#inputVersion').val();
+   var assetId =  $('#inputId').val();
+
+
+    $.ajax({
+        url: '/publisher/api/asset/copy/'+ assetId,
+        type: 'POST',
+        contentType: 'application/json',
+        data: {newVersion: newVersion},
+        dataType: 'json',
+        success: function(response) {
+            alert(response);
+        },
+        error: function(response) {
+            //showAlert('Error occured while fetching entitlement policy content', 'error');
+        }
+    });
+
+
+
 });
