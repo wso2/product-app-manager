3be84fe449b778c173a4e1ecd8965a0e0f9b1b49
Chatura Dilan <dilan@dilan.me>
2014-10-10 11:06:42 +0530

    emm canges

--- modules/apps/publisher/themes/appm/js/entitlement.js
+++ modules/apps/publisher/themes/appm/js/entitlement.js
@@ -67,8 +67,7 @@ $(document).on("click", "#btn-policy-save", function () {
         alert("fields cannot be blank");
         return;
     }
-    validatePolicyPartial(policyContent, continueAddingEntitlementPolicyPartialAfterValidation,
-                                                            displayValidationRequestException);
+    validatePolicyPartial(policyContent, continueAddingEntitlementPolicyPartialAfterValidation, displayValidationRequestException);
 
 
 });
@@ -451,14 +450,16 @@ function updatePolicyPartial(){
 
         $('.uritemplate_entitlementPolicyPartialMappings_text').each(function(i, obj) {
             var values = JSON.parse($(this).val());
-
-            for (var j = 0; j < values.length; j++){
-                if(values[j].effect == "Permit"){
-                    $('#dropdown_entitlementPolicyPartialMappings' + (uriTemplates - i - 1) + " .policy-allow-cb" + values[j].entitlementPolicyPartialId).prop('checked', true);
-                }else if (values[j].effect == "Deny"){
-                    $('#dropdown_entitlementPolicyPartialMappings' + (uriTemplates - i - 1) + " .policy-deny-cb" + values[j].entitlementPolicyPartialId).prop('checked', true);
+            if(values){
+                for (var j = 0; j < values.length; j++){
+                    if(values[j].effect == "Permit"){
+                        $('#dropdown_entitlementPolicyPartialMappings' + (uriTemplates - i - 1) + " .policy-allow-cb" + values[j].entitlementPolicyPartialId).prop('checked', true);
+                    }else if (values[j].effect == "Deny"){
+                        $('#dropdown_entitlementPolicyPartialMappings' + (uriTemplates - i - 1) + " .policy-deny-cb" + values[j].entitlementPolicyPartialId).prop('checked', true);
+                    }
                 }
             }
+
         });
 
 
