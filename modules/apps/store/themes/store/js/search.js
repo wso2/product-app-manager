2b29657b422989b920be219cc0159049d2f6c79b
Chatura Dilan <dilan@dilan.me>
2014-10-16 16:33:04 +0530

    Fixed search in mobile store

--- modules/apps/store/themes/store/js/search.js
+++ modules/apps/store/themes/store/js/search.js
@@ -132,6 +132,7 @@ $(function () {
                 url: url,
                 success: function (data, status, xhr) {
                     //TODO: Integrate a new History.js library to fix this
+                    window.location.replace("/store/assets/mobileapp/?query=" + searchVal);
                     if ($.browser.msie == true && $.browser.version < 10) {
                         renderAssets(data);
                     } else {
