4e19e7fa34c3e5cad198a6550987ace5c22b8101
binalip91 <binalip91@gmail.com>
2015-02-11 09:12:45 +0530

    app_sub_user.js

---
+++ modules/jaggery-apps/app-publisher-web/themes/appm/helpers/app_sub_user.js
@@ -0,0 +1,6 @@
+var resources = function (page,meta) {
+    return {
+        js: ['typeahead.jquery.js','flot/jquery.flot.js', 'flot/excanvas.min.js', 'logic/flot.stat.appsubuser.js','date_picker/datepicker.js','logic/date.picker.js','flot/jquery.flot.pie.js','flot/jquery.flot.axislabels.js','flot/jquery.flot.orderBars.js','flot/excanvas.js','flot/jquery.flot.tooltip.js','jquery.dialogextend.js','logic/left-nav.js','d3.v3.js','nv.d3.js'],
+        css: ['flot-graphs.css','date_picker/datepicker/base.css','date_picker/datepicker/custom-widget.css','date_picker/datepicker/clean.css','search-container.css','nv.d3.css']
+    };
+};
