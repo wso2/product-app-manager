7644b3aba7a112c9afa6c89c30030ec6922ee876
Chatura Dilan <dilan@dilan.me>
2015-03-11 17:07:00 +0530

    Changes to mdm configurations

--- modules/jaggery-apps/app-store-web/extensions/assets/mobileapp/modules/mod-ent-install.js
+++ modules/jaggery-apps/app-store-web/extensions/assets/mobileapp/modules/mod-ent-install.js
@@ -1,15 +1,20 @@
     var log =  new Log();
 
-
-
-
     var performAction = function performAction (action, tenantId, type, app, params) {
 
+        if( typeof params === 'string' ) {
+            params = [ params ];
+        }
+
         log.debug(action + " performs on the app " + app + " in tenant " + tenantId + " & " + type + " : " + stringify(params));
-        var  server = require('store').server,
-                store = require('/modules/store.js').store(tenantId, session);
-        asset = store.asset('mobileapp', app);
-        log.debug("Asset retrieved : " + stringify(asset));
+//        var  server = require('store').server,
+//                store = require('/modules/store.js').store(tenantId, session);
+//        asset = store.asset('mobileapp', app);
+//        log.debug("Asset retrieved : " + stringify(asset));
+
+        var operationsClass = Packages.org.wso2.carbon.appmgt.mobile.store.Operations;
+        var operations = new operationsClass();
+        operations.performAction(action, tenantId, type, app, params);
 
     };
 
