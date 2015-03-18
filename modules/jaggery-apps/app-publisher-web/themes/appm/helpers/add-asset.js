966577c610b0daf4a0eb5d0cf470fd094543970a
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-03-18 15:33:42 +0530

    add placeholder text to webapps

--- modules/jaggery-apps/app-publisher-web/themes/appm/helpers/add-asset.js
+++ modules/jaggery-apps/app-publisher-web/themes/appm/helpers/add-asset.js
@@ -1,6 +1,8 @@
 var resources = function (page, meta) {
     return {
-        js: ['logic/create.asset.js', 'bootstrap-select.min.js','options.text.js',  'codemirror/codemirror.js', 'codemirror/show-hint.js', 'codemirror/xml-hint.js', 'codemirror/xml.js', 'resource-add.js', 'entitlement.js', 'policy.group.js', 'java.policy.js'],
+        js: ['logic/create.asset.js', 'bootstrap-select.min.js','options.text.js',  'codemirror/codemirror.js',
+            'codemirror/show-hint.js', 'codemirror/xml-hint.js', 'codemirror/xml.js', 'resource-add.js',
+            'entitlement.js', 'policy.group.js', 'java.policy.js', 'publisher.placeholder.js'],
         css:['bootstrap-select.min.css', 'datepick/smoothness.datepick.css',  'codemirror/codemirror.css']
     };
 };
