f1cdb8cfbfc5a308a50660706ea018fe1686ad09
khsushan <khsushan@gmail.com>
2015-06-22 09:15:52 +0530

    add updated header files

--- modules/integration/tests-ui/src/test/java/org/wso2/carbon/appmanager/integration/ui/Util/service/AuthenticationAdmin.java
+++ modules/integration/tests-ui/src/test/java/org/wso2/carbon/appmanager/integration/ui/Util/service/AuthenticationAdmin.java
@@ -1,3 +1,23 @@
+/*
+ *
+ *   Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
 package org.wso2.carbon.appmanager.integration.ui.Util.service;
 
 import org.apache.axis2.context.ServiceContext;
@@ -5,16 +25,21 @@ import org.apache.axis2.transport.http.HTTPConstants;
 import org.wso2.carbon.authenticator.stub.AuthenticationAdminStub;
 
 /**
- * Created by ushan on 6/11/15.
+ * This class is use as a client for Authentication admin service
  */
 public class AuthenticationAdmin {
     private static final String SERVICE_NAME = "AuthenticationAdmin";
 
+    /**
+     * This method is use to get authentication cookie to call services
+     *
+     * @param endpoint -backend server url
+     */
     public static String getCookie(String endpoint) {
 
         String cookie = null;
         try {
-            AuthenticationAdminStub authAdminStub = new AuthenticationAdminStub(endpoint+SERVICE_NAME);
+            AuthenticationAdminStub authAdminStub = new AuthenticationAdminStub(endpoint + SERVICE_NAME);
             boolean loginSuccess = authAdminStub.login("admin", "admin", "localhost");
 
             if (loginSuccess) {
