640c7d3cbbb42b47645b2bd35ee521ac9bb2b103
tgtshanika <tgtshanika@gmail.com>
2016-01-29 11:15:54 +0530

    Adding DeleteWebAppInUnpublishedStateTestCase

---
+++ modules/integration/tests-ui/src/test/java/org/wso2/appmanager/ui/integration/test/cases/publisher/webapp/delete/DeleteWebAppInUnpublishedStateTestCase.java
@@ -0,0 +1,7 @@
+package org.wso2.appmanager.ui.integration.test.cases.publisher.webapp.delete;
+
+/**
+ * Created by thilini on 1/29/16.
+ */
+public class DeleteWebAppInUnpublishedStateTestCase {
+}
