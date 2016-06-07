421f21d05b156f6aa99c1249594eb625e8f50aac
sajithab <sajitha@wso2.com>
2016-06-07 19:15:48 +0530

    Cleanup on CAR file
    Cleanup on CAR file

--- modules/distribution/resources/migrations/Migration-client/src/main/java/org/wso2/carbon/appmgt/migration/util/MigratingWebApp.java
+++ modules/distribution/resources/migrations/Migration-client/src/main/java/org/wso2/carbon/appmgt/migration/util/MigratingWebApp.java
@@ -1,47 +1,92 @@
+/*
+* Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.wso2.carbon.appmgt.migration.util;
 
-import org.wso2.carbon.appmgt.api.model.APIIdentifier;
-import org.wso2.carbon.appmgt.api.model.WebApp;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+import org.wso2.carbon.registry.core.Registry;
+import org.wso2.carbon.registry.core.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MigratingWebApp {
-    private Date createdTime;
-    private WebApp webApp;
+public class MigratingWebapp {
+    private static final Log log = LogFactory.getLog(MigratingWebapp.class);
 
-    public WebApp getWebApp() {
-        return webApp;
-    }
-
-    public MigratingWebApp(WebApp webApp) {
-        this.webApp = webApp;
-    }
+    private String appName;
+    private String appVersion;
+    private String appProvider;
+    private String lcState;
+    private Date createdTimeStamp;
 
     public boolean isPublished() {
-        boolean isPublished = false;
-        if ("PUBLISHED".equals(webApp.getStatus().getStatus())) {
-           isPublished = true;
-        }
         return isPublished;
     }
 
-    public String getAppName(){
-        return getId().getApiName();
+    public void setPublished(boolean isPublished) {
+        this.isPublished = isPublished;
     }
 
-    public String getVersion(){
-        return getId().getVersion();
+    private boolean isPublished;
+    public String getAppName() {
+        return appName;
     }
 
-    public APIIdentifier getId(){
-        return webApp.getId();
+    public void setAppName(String appName) {
+        this.appName = appName;
     }
 
-    public Date getCreatedTime() {
-        return createdTime;
+    public String getAppVersion() {
+        return appVersion;
     }
 
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getAppProvider() {
+        return appProvider;
+    }
+
+    public void setAppProvider(String appProvider) {
+        this.appProvider = appProvider;
+    }
+
+    public String getLcState() {
+        return lcState;
+    }
+
+    public void setLcState(String lcState) {
+        this.lcState = lcState;
+        if("Published".equals(lcState)){
+            setPublished(true);
+        }else{
+           setPublished(false);
+        }
+    }
+
+    public Date getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public void setCreatedTimeStamp(Date createdTimeStamp) {
+        this.createdTimeStamp = createdTimeStamp;
     }
 }
